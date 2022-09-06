dcdc6dc4e4cc9cc6b9c041560651942bcc436de8
Matthew Setter <matthew@matthewsetter.com>
2022-09-06 13:27:59 +0200

    Add the mock-up of the application
    This, the first change, adds the application mock-up. It has extremely
    minimal functionality, but enough to give an idea of what will be
    available when the application is complete.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'spidersweb': "url('/images/backgrounds/spidersweb.png')"
+      },
+      colors: {
+      }
+    }
+  },
+  plugins: [],
+}
